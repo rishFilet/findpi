e97bc83db9f9ebe4d1304f3d63af1aadf4b7eead
Rishi Khan <rishi.rkhan@gmail.com>
2018-12-12 17:31:07 -0500

    Updated up until the point of reading the ip addresses and putting them in nmap

--- findpi.sh
+++ findpi.sh
@@ -1,10 +1,27 @@
-#!/bin/bash
-command="`nmap -sP 0.0.0.0/24 255.0.0.0/24`"
+command="`ifconfig`"
+declare -a ips
 while read line
 do
   alllines="$line"
-  if [[ $line =~ "westworlds" ]]
-  then lastline="$line"
-  echo "$lastline"
-fi
+  if [[ $line =~ "inet" ]]
+  then
+    if [[ $line != *"inet6"* ]]
+    then
+      if [[ $line != *"127"* ]]
+      then
+        lastline="$line"
+        ip="${lastline##*inet}"
+        ip="${ip%netmask*}"
+        echo ${ip}
+        ips+=(${ip})
+      fi
+    fi
+  fi
 done <<< "$command"
+
+echo "${ips[0]%.*}"
+
+test="${ips[0]%.*}"
+test2=${test}".0/24"
+echo ${test2}
+echo "Mutantx1" | sudo -S nmap -sP ${test2}
