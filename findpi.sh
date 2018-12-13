8d7acebc93e5910803bc9f0a7f22b523becb433f
Rishi Khan <rishi.rkhan@gmail.com>
2018-12-13 14:55:10 -0500

    Got up to the point of finding the pi and ssh
    Still need to ssh into multiple ips in windows and read their outputs for the command

--- findpi.sh
+++ findpi.sh
@@ -1,8 +1,9 @@
 command="`ifconfig`"
+echo "Executing ifconfig command to gather connected ips...."
 declare -a ips
 while read line
 do
-  alllines="$line"
+  #alllines="$line"
   if [[ $line =~ "inet" ]]
   then
     if [[ $line != *"inet6"* ]]
@@ -12,16 +13,46 @@ do
         lastline="$line"
         ip="${lastline##*inet}"
         ip="${ip%netmask*}"
-        echo ${ip}
+        #echo ${ip}
+        ip="${ip%.*}"
         ips+=(${ip})
       fi
     fi
   fi
 done <<< "$command"
 
-echo "${ips[0]%.*}"
 
-test="${ips[0]%.*}"
-test2=${test}".0/24"
-echo ${test2}
-echo "Mutantx1" | sudo -S nmap -sP ${test2}
+declare -a raspips
+echo "Gathering ip addresses using nmap.."
+for ip in "${ips[@]}"
+do
+  command="`echo "Mutantx1" | sudo -S nmap -sP ${ip}".0/24"`"
+  while read line
+  do
+    if [[ $line =~ "Nmap scan report" ]]
+    then
+      ipline="${line##*for}"
+    fi
+    if [[ $line =~ "Raspberry Pi" ]]
+    then
+      raspips+=(${ipline})
+    fi
+  done <<< "$command"
+done
+
+for ip in "${raspips[@]}"
+do
+  #command="`ssh -tt pi@${ip}`"
+  ttab ssh -tt pi@${ip}
+  while read line
+  do
+    if [[ $line =~ "Are you sure you want to continue connecting" ]]
+    then
+      echo "yes"
+    fi
+    if [[ $line =~ "pi@" ]]
+    then
+      echo "raspberry"
+    fi
+  done <<< "$command"
+done
